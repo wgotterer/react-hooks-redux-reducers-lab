b68e83e07959653e0850ca4f8307c5a9718ff6a8
William Gotterer <william.gotterer@gmail.com>
2022-01-12 18:29:26 -0500

    Done.

--- src/manageFriends.js
+++ src/manageFriends.js
@@ -1,3 +1,31 @@
 export function manageFriends(state, action) {
-  // your code here
+  switch(action.type){
+    case("friends/add") :
+      return {friends: [...state.friends, action.payload] }
+    case("friends/remove") :
+      return{ friends: [...state.friends].filter(friend =>{
+        return friend.id !== action.payload
+      })   
+      }
+      default:
+        return state
+  }
+ 
+}
+
+let state = {friends: []}
+
+let action = {
+  type: "friends/add",
+  payload: {
+    name: "Chrome Boi",
+    hometown: "NYC",
+    id: 1
+  }
+
+}
+
+  action = {
+  type: "friends/remove",
+  payload: 1,
 }
--- src/managePresents.js
+++ src/managePresents.js
@@ -1,3 +1,15 @@
 export function managePresents(state, action) {
-  // your code here
+  switch(action.type){
+    case "presents/increase" :
+      return {numberOfPresents: state.numberOfPresents + 1}
+      default:
+        return state
+  }
 }
+
+let state = {numberOfPresents: 0}
+
+let action = {
+  type: "presents/increase"
+}
+
